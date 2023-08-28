01860d61c9c948062f02e375e21e4144879a0ea9
Aditi <aditipatiyal08@gmail.com>
2023-08-28 15:12:31 +0530

    change

--- scholar_shift/src/components/Header.js
+++ scholar_shift/src/components/Header.js
@@ -9,13 +9,13 @@ const Header = () => {
     return (
         <div className='header'>
             <div className='container'>
-                <nav className="navbar navbar-expand-lg navbar-light">
+                <nav className="navbar navbar-expand-lg navbar-light ">
                     <Link className="navbar-brand" to='/'><img src={Logo} alt='logo'></img></Link>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
 
-                    <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav ms-auto mt-2 mt-lg-0">
                             <li className="nav-item">
                                 <Link className="nav-link px-3" to='/' href="#">Home</Link>
